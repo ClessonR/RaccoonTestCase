11062d3303e67a29325e05971948b55eb6ff793d
ClessonR <bebettus2009@live.com>
2022-06-14 18:23:47 -0300

    Adding mving average

---
+++ TestCase_1/movingaverage.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("Data/full_data.csv", index_col='data', parse_dates=True)
+
+
+df.drop(["transacoes_blog",
+                        "transacoes_site",
+                        "usuarios_blog",
+                        "usuarios_site"], axis=1, inplace=True)
+
+
+df.plot(color='green', linewidth=3, figsize=(12,6))
+df['receita'].rolling(window=72).mean().plot()
+
+
+
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.legend()
+
+plt.title('The average revenue per day')
+plt.xlabel("Date")
+plt.ylabel('Revenue [R$]')
+plt.show()
