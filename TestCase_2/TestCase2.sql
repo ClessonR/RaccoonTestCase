885275fa511c03653fdb8796a3554da21f907d34
Clesson Roberto da Silva Júnior <53549230+ClessonR@users.noreply.github.com>
2022-06-16 15:47:19 -0300

    Update TestCase2.sql

--- TestCase_2/TestCase2.sql
+++ TestCase_2/TestCase2.sql
@@ -1,17 +1,17 @@
--- Qual foi a receita de cada tipo de pagamento no dia 15 de Mar�o de 2018? OK
+-- Qual foi a receita de cada tipo de pagamento no dia 15 de Marco de 2018? OK
 
 select SUM(total_amount),payment_type from dbo.testcase 
 WHERE cast(dropoff_datetime as date) = '2018-03-15' 
 GROUP BY payment_type; 
 
--- Considere que corridas de taxi validas tenham de 1 a 5 passageiros. Qual a quantidade de corridas feitas com cada n�mero de passageiros?  OK
+-- Considere que corridas de taxi validas tenham de 1 a 5 passageiros. Qual a quantidade de corridas feitas com cada numero de passageiros?  OK
 
 select COUNT(*) as taxi_runs, passenger_count as number_of_passangers from dbo.testcase 
 where passenger_count between 1 and 5 
 GROUP BY passenger_count
 ORDER BY passenger_count;
 
--- Considerando apenas as corridas que houveram pedagios (tolls), qual a m�dia do valor pago em ped�gios por corrida? OK
+-- Considerando apenas as corridas que houveram pedagios (tolls), qual a media do valor pago em pedagios por corrida? OK
 
 select SUM(tolls_amount)/COUNT(*) from dbo.testcase 
 where tolls_amount > 0;
